d3a4d4460eca30893c8cb764e9276a77245ad690
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-04-12 15:49:31 +0200

    Rewrite import

--- bin/import.js
+++ bin/import.js
@@ -1,33 +1,15 @@
 #!/usr/bin/env node
 const {join} = require('path')
-const bluebird = require('bluebird')
-const mongo = require('../lib/utils/mongo')
-const loadBAN = require('../build/load/ban')
-const loadBANO = require('../build/load/bano')
-const loadCadastre = require('../build/load/cadastre')
+
+const doImport = require('../lib/import')
 
 const DATA_DIR = join(__dirname, '..', 'data')
 const BAN_PATH = join(DATA_DIR, '54_ban_v0.csv.gz')
 const BANO_PATH = join(DATA_DIR, 'bano-54.csv.gz')
 const CADASTRE_PATH = join(DATA_DIR, 'adresses-cadastre-54.geojson.gz')
 
-async function main() {
-  await mongo.connect()
-
-  const adresses = await bluebird.props({
-    ban: loadBAN(BAN_PATH),
-    bano: loadBANO(BANO_PATH),
-    cadastre: loadCadastre(CADASTRE_PATH)
+doImport({banPath: BAN_PATH, banoPath: BANO_PATH, cadastrePath: CADASTRE_PATH})
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
   })
-
-  await mongo.db.collection('adresses').insertMany(adresses.ban)
-  await mongo.db.collection('adresses').insertMany(adresses.bano)
-  await mongo.db.collection('adresses').insertMany(adresses.cadastre)
-
-  await mongo.disconnect()
-}
-
-main().catch(err => {
-  console.error(err)
-  process.exit(1)
-})
