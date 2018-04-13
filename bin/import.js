5998ec72b34ff4ff3788d0ae0f814417806cc424
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-04-13 22:32:37 +0200

    Consolidate everything

--- bin/import.js
+++ bin/import.js
@@ -6,7 +6,7 @@ const doImport = require('../lib/import')
 const DATA_DIR = join(__dirname, '..', 'data')
 const BAN_PATH = join(DATA_DIR, '54_ban_v0.csv.gz')
 const BANO_PATH = join(DATA_DIR, 'bano-54.csv.gz')
-const CADASTRE_PATH = join(DATA_DIR, 'adresses-cadastre-54.geojson.gz')
+const CADASTRE_PATH = join(DATA_DIR, 'adresses-cadastre-54.ndjson.gz')
 
 doImport({banPath: BAN_PATH, banoPath: BANO_PATH, cadastrePath: CADASTRE_PATH})
   .catch(err => {
