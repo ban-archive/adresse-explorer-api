a887e2b728cb55ea9180deb37efe795ca0eb64ca
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-04-23 21:12:16 +0200

    Add ban-v0 loader

--- bin/import.js
+++ bin/import.js
@@ -4,7 +4,7 @@ const {join} = require('path')
 const doImport = require('../lib/import')
 
 const DATA_DIR = join(__dirname, '..', 'data')
-const BAN_PATH = join(DATA_DIR, '54_ban_v0.csv.gz')
+const BAN_PATH = join(DATA_DIR, 'BAN_licence_gratuite_repartage_54.zip')
 const BANO_PATH = join(DATA_DIR, 'bano-54.csv.gz')
 const CADASTRE_PATH = join(DATA_DIR, 'adresses-cadastre-54.ndjson.gz')
 
