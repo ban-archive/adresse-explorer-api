cc4f6f1bba871ea9ce4d340024a7615aa0d29707
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-04-24 15:18:30 +0200

    Wrap MongoDB import in condition

--- bin/import.js
+++ bin/import.js
@@ -8,7 +8,7 @@ const BAN_PATH = join(DATA_DIR, 'BAN_licence_gratuite_repartage_54.zip')
 const BANO_PATH = join(DATA_DIR, 'bano-54.csv.gz')
 const CADASTRE_PATH = join(DATA_DIR, 'adresses-cadastre-54.ndjson.gz')
 
-doImport({banPath: BAN_PATH, banoPath: BANO_PATH, cadastrePath: CADASTRE_PATH})
+doImport({banPath: BAN_PATH, banoPath: BANO_PATH, cadastrePath: CADASTRE_PATH, importInMongo: false})
   .catch(err => {
     console.error(err)
     process.exit(1)
