462c25e85cf459159b0da5a240e3278f7d008eac
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-04-24 20:54:53 +0200

    Write addok data to file

--- bin/import.js
+++ bin/import.js
@@ -7,8 +7,9 @@ const DATA_DIR = join(__dirname, '..', 'data')
 const BAN_PATH = join(DATA_DIR, 'BAN_licence_gratuite_repartage_54.zip')
 const BANO_PATH = join(DATA_DIR, 'bano-54.csv.gz')
 const CADASTRE_PATH = join(DATA_DIR, 'adresses-cadastre-54.ndjson.gz')
+const ADDOK_FILE_PATH = join(DATA_DIR, 'addok-source-files', '54.ndjson.gz')
 
-doImport({banPath: BAN_PATH, banoPath: BANO_PATH, cadastrePath: CADASTRE_PATH, importInMongo: false, writeAddokFile: true})
+doImport({banPath: BAN_PATH, banoPath: BANO_PATH, cadastrePath: CADASTRE_PATH, importInMongo: false, addokFilePath: ADDOK_FILE_PATH})
   .catch(err => {
     console.error(err)
     process.exit(1)
