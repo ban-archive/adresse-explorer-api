489c7f8ca0b60a2f7f9f41f26a432d56453bd483
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-04-24 22:39:30 +0200

    Add addok helpers

---
+++ bin/prepare-addok.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "> Démontage de l'environnement docker (le cas échéant)"
+docker-compose -f docker/prepare-addok.yml stop
+
+echo "> Suppression des anciennes données"
+rm -Rf data/addok-data
+
+echo "> Importation des données dans Redis"
+gzcat data/addok-source-files/*.ndjson.gz | docker-compose -f docker/prepare-addok.yml run addok-importer batch
+
+echo "> Création des ngrams"
+docker-compose -f docker/prepare-addok.yml run addok-importer ngrams
+
+echo "> Démontage de l'environnement docker"
+docker-compose -f docker/prepare-addok.yml stop
+
+echo "> Listing des fichiers produits"
+ls -lh data/addok-data
+
+echo "Terminé"
