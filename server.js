82dcdadc134ac7411f76a5ff8070ef2c21561eb0
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-01-04 21:45:20 +0100

    Remove API BAN proxy

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const {createClient} = require('./lib/ban/client')
 const wrap = require('./lib/utils/wrap')
 const mongo = require('./lib/utils/mongo')
 const db = require('./lib/models')
@@ -16,24 +15,6 @@ function badRequest(message) {
 }
 
 app.use(cors())
-
-const client = createClient({
-  clientId: process.env.BAN_CLIENT_ID,
-  clientSecret: process.env.BAN_CLIENT_SECRET,
-  baseUrl: process.env.BAN_API_URL || 'https://api-ban.ign.fr'
-})
-
-app.get('/ban/communes/:codeCommune', wrap(req => {
-  return client.getCommune(req.params.codeCommune)
-}))
-
-app.get('/ban/communes/:codeCommune/voies', wrap(req => {
-  return client.getVoies(req.params.codeCommune)
-}))
-
-app.get('/ban/voies/:id', wrap(req => {
-  return client.getNumerosVoie(req.params.id)
-}))
 
 app.get('/explore/:codeCommune', wrap(async req => {
   const voies = await db.getVoies(req.params.codeCommune)
